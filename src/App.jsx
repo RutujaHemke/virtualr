1dbe0f2f9e08c222085fa38069ce30949adda604
Rutuja Hemke <rutujahemke07@.com>
2026-03-09 14:49:38 +0530

    workflow section - fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Workflow from "./Components/Workflow"
 import FeatureSection from "./Components/FeatureSection"
 import HeroSection from "./Components/HeroSection"
 import Navbar from "./Components/Navbar"
@@ -12,6 +12,7 @@ function App() {
      <div className="max-w-7xl max-auto pt-20 px-20">
       <HeroSection />
       <FeatureSection />
+      <Workflow />
      </div>
      
     </>
